c8a4b22d95a5e1d108aa4ba9a93aa459bc4b184e
Андрей <nevraev1109@mail.ru>
2022-08-29 18:19:44 +0500

    Add project files.

---
+++ Discipline/Class1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.IO;
+using System.Windows;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateLab
+{
+    class Journal
+    {
+        public static string NameOfStudent = "John Dow", Discipline = "Discipline";
+        public static byte Mark = 0;
+        public static DateTime DateOfMark = DateTime.Now;
+        private static string path = "Journal.txt";
+        //private object ClipboardService;
+        public void SetDiscipline(string _title)
+        {
+            Discipline = _title;
+        }
+        public void PrintDiscipline()
+        {
+            Console.WriteLine(Discipline);
+        }
+        public void SetName(string _name)
+        {
+            NameOfStudent = _name;
+        }
+        public string GetName()
+        {
+            return NameOfStudent;
+        }
+        public void PrintName()
+        {
+            Console.WriteLine(NameOfStudent);
+        }
+        public void WriteNameToFile()
+        {
+            string myDocs = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            path = myDocs + "\\" + path;
+
+            using (StreamWriter sw = File.AppendText(path))
+                sw.WriteLine(NameOfStudent);
+           
+
+
+            Console.WriteLine("Информация добавлена в файл {0}.", path);
+            
+        }
+        public void WriteDisciplineToFile()
+        {
+            string myDocs = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            path = myDocs + "\\" + path;
+            using (StreamWriter sw = File.AppendText(path))
+                sw.WriteLine(Discipline);
+           
+
+            Console.WriteLine("Информация добавлена в файл {0}.", path);
+        }
+        
+        /*public void CopyToClipboard()
+        {
+            //CopyToClipboard.SetText("Hello, clipboard");
+            ClipboardService.SetText(NameOfStudent);
+            Console.WriteLine("Информация добавлена в буфер обмена.");
+        }
+        public string GetFromClipboard()
+        {
+            return ClipboardService.GetText();
+        }*/
+    }
+}
---
+++ Discipline/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateLab
+{
+    class Program
+    {
+        delegate void Output();
+       // delegate void Output2();
+        static void Choice2(Journal myJournal)
+        {
+            Output _myOutput2;
+            Console.WriteLine("Вывести на экран? (Y) На экран и в файл (F)");
+            string answer = Console.ReadLine();
+            if (answer == "Y" | answer == "y")
+            {
+
+                _myOutput2 = myJournal.PrintDiscipline;
+
+            }
+            else
+            {
+                _myOutput2 = myJournal.WriteDisciplineToFile;
+
+
+            }
+            _myOutput2 -= myJournal.PrintDiscipline;
+            _myOutput2 += myJournal.PrintDiscipline;
+            _myOutput2();
+        }
+            static void Choice(Journal myJournal)
+        {
+            Output _myOutput;
+            Console.WriteLine("Вывести на экран? (Y) На экран и в файл (F)");
+            string answer = Console.ReadLine();
+            if (answer == "Y" | answer == "y")
+            {
+                
+                _myOutput = myJournal.PrintName;
+                _myOutput += myJournal.PrintDiscipline;
+            }
+            else
+            {
+                if (answer == "F" | answer == "f")
+                {
+                    _myOutput = myJournal.PrintName;
+                    _myOutput += myJournal.WriteNameToFile;
+
+                }
+                else
+                {
+                    //_myOutput = myJournal.WriteNameToFile;
+                    _myOutput = myJournal.WriteDisciplineToFile;
+
+                }
+
+            }
+           
+                //_myOutput += myJournal.CopyToClipboard;
+                //_myOutput += () => Console.WriteLine("Hello");
+           
+            _myOutput();
+        }
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Введите имя студента: ");
+            Console.WriteLine("Введите предмет: ");
+           
+
+            var myJournal = new Journal();
+            
+
+            myJournal.SetName(Console.ReadLine());
+            myJournal.SetDiscipline(Console.ReadLine());
+            Choice(myJournal);
+            // var myJournal2 = new Journal();
+
+           
+           
+            
+           // Choice2(myJournal);
+
+           /* Console.WriteLine(myJournal.GetFromClipboard());
+            Output func = () =>
+            {
+                Console.WriteLine("Hello");
+                Console.WriteLine("Hello".Length);
+            };
+            func();
+            var func001(string _text) => Console.WriteLine(_text);*/
+            Console.ReadKey();
+        }
+    }
+}
+
